7f3174fd64d8604a9386b146676aaf89de0f2177
MohitSingh7979 <mohitsinghatwork@gmail.com>
2024-08-04 02:30:47 +0530

    fresh start

--- src/doubleframe/DoubleFrame.java
+++ src/doubleframe/DoubleFrame.java
@@ -3,19 +3,6 @@ package doubleframe;
 public class DoubleFrame {
 
   public static void main(String[] args) {
-    m();
+
   }
-
-  static void m() {
-    Something s = new Something();
-    System.out.println(s);
-  }
-
-
-  private static class Something {
-
-    public Something() {
-    }
-  }
-
 }
