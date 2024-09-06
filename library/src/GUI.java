ff5955c1ead18f61390b363518987192a162e3e8
MohitSingh7979 <mohitsinghatwork@gmail.com>
2024-09-06 21:21:06 +0530

    initialize sql connection

--- library/src/GUI.java
+++ library/src/GUI.java
@@ -1,7 +1,19 @@
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class GUI extends javax.swing.JFrame {
+
+    Connection con;
+    String sql;
 
     public GUI() {
         initComponents();
+        initializeConnection();
     }
 
     @SuppressWarnings("unchecked")
@@ -34,4 +46,12 @@ public class GUI extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
+    void initializeConnection() {
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","root");
+            System.out.println("connection");
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
