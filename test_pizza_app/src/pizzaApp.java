ec153c8a822ebeac718bd52e90c6edd6c800e684
Mohit Singh <mohitsinghatwork@gmail.com>
2024-10-01 14:44:38 +0530

    fillpizzaname

--- test_pizza_app/src/pizzaApp.java
+++ test_pizza_app/src/pizzaApp.java
@@ -1,7 +1,42 @@
+import java.sql.*;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class pizzaApp extends javax.swing.JFrame {
+Connection con;
+PreparedStatement ps;
+String sql;
 
     public pizzaApp() {
         initComponents();
+        initConnection();
+        fillPizzaName();
+
+    }
+
+    private void fillPizzaName() {
+        boolean isVeg = veg.isSelected() ;
+        sql="SELECT * FROM pizza_master pm where is_veg = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setBoolean(1, isVeg);
+            ResultSet rs = ps.executeQuery();
+            pizza.removeAllItems();
+            while (rs.next()) {
+                pizza.addItem(("%s-%s").formatted(rs.getString(1),rs.getString(2)));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(pizzaApp.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void initConnection() {
+        try {
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test_pizza_app", "root", "root");
+        } catch (SQLException ex) {
+            Logger.getLogger(pizzaApp.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @SuppressWarnings("unchecked")
@@ -27,6 +62,8 @@ public class pizzaApp extends javax.swing.JFrame {
         report1 = new javax.swing.JTable();
         jScrollPane2 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
+        javax.swing.JLabel jLabel8 = new javax.swing.JLabel();
+        veg = new javax.swing.JCheckBox();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -45,6 +82,11 @@ public class pizzaApp extends javax.swing.JFrame {
         jLabel7.setText("SAVING");
 
         orderPizzaBtn.setText("ORDER PIZZA");
+        orderPizzaBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                orderPizzaBtnActionPerformed(evt);
+            }
+        });
 
         pizza.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
@@ -74,6 +116,15 @@ public class pizzaApp extends javax.swing.JFrame {
         ));
         jScrollPane2.setViewportView(jTable1);
 
+        jLabel8.setText("VEG");
+
+        veg.setText("Yes");
+        veg.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                vegStateChanged(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -94,7 +145,11 @@ public class pizzaApp extends javax.swing.JFrame {
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(quantity, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addComponent(quantity, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(veg, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)))
                         .addGap(18, 18, 18)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 456, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
@@ -131,6 +186,10 @@ public class pizzaApp extends javax.swing.JFrame {
                             .addComponent(id, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(veg, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(pizza, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(18, 18, 18)
@@ -165,6 +224,14 @@ public class pizzaApp extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void vegStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_vegStateChanged
+        fillPizzaName();
+    }//GEN-LAST:event_vegStateChanged
+
+    private void orderPizzaBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_orderPizzaBtnActionPerformed
+        
+    }//GEN-LAST:event_orderPizzaBtnActionPerformed
+
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -185,5 +252,6 @@ public class pizzaApp extends javax.swing.JFrame {
     private javax.swing.JSpinner quantity;
     private javax.swing.JTable report1;
     private javax.swing.JSpinner saving;
+    private javax.swing.JCheckBox veg;
     // End of variables declaration//GEN-END:variables
 }
